89796c3bc4e5e9f93515385af0de787866c913bd
kinal <kinal.mehta11@gmail.com>
2022-03-25 19:14:33 +0530

    fedora and ubuntu setup scripts/commands

---
+++ fedora_setup.sh
@@ -0,0 +1,38 @@
+
+sudo dnf update -y
+sudo dnf upgrade --refresh
+
+sudo dnf groupinstall 'Development Tools'
+sudo dnf install gcc-c++
+
+
+# installing nvidia drivers
+sudo dnf install kernel-devel
+sudo dnf install akmod-nvidia
+sudo systemctl enable nvidia-hibernate.service nvidia-suspend.service nvidia-resume.service
+
+# setting blacklist novou and other setting mentioned in asus-linux
+sudo gedit /etc/default/grub
+sudo grub2-mkconfig -o /etc/grub2.cfg
+
+
+# zsh install
+sudo dnf install zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # "" --skip-chsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+sed -i 's#plugins=(git)#plugins=( \n git \n colored-man-pages \n zsh-autosuggestions \n zsh-history-substring-search \n zsh-syntax-highlighting \n )#g' $HOME/.zshrc
+
+# set zsh as the default shell
+chsh -s $(which zsh)
+
+
+### Installing Miniconda for python development
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -bfu  # b is batch mode, f is no error if already installed, u is update existing installation
+PREFIX=$HOME/miniconda3
+$PREFIX/bin/conda init
+
