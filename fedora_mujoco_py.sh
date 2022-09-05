8f93be742ae6e0356374cfb80f7c1e4d185c74d4
Kinal Mehta <kinal.mehta11@gmail.com>
2022-09-05 12:29:29 +0530

    add conda install commands for mujoco-py old

--- fedora_mujoco_py.sh
+++ fedora_mujoco_py.sh
@@ -1,1 +1,5 @@
 sudo yum install -y make gcc-c++ mesa-libGL-devel mesa-libOSMesa-devel glfw redhat-rpm-config patchelf
+
+# optional if above doesn't work
+conda install -c conda-forge gcc gxx ffmpeg
+conda install -c conda-forge glew mesalib
