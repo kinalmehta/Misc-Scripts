731244f4704b4ed51b1063d48ab499741fb06f3f
Kinal Mehta <kinal.mehta11@gmail.com>
2022-09-08 19:14:07 +0530

    add reference for sdl

--- fedora_mujoco_py.sh
+++ fedora_mujoco_py.sh
@@ -6,5 +6,6 @@ conda install -c conda-forge glew mesalib
 
 
 # google football dependencies
+# Ref: https://github.com/vlang/sdl#fedora
 sudo dnf install SDL2-devel SDL2_ttf-devel SDL2_mixer-devel SDL2_image-devel SDL2_gfx-devel SDL2_net-devel SDL2_sound-devel
 conda install -c conda-forge boost
